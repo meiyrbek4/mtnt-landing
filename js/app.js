293b2ad5abc4b404c7b530171c36cbac6644ddb6
meiyrbek4 <meiyrbek@gmail.com>
2025-03-07 23:14:57 +0500

    initial commit

---
+++ js/app.js
@@ -0,0 +1,24 @@
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+if (ScrollTrigger.isTouch !== 1) {
+  ScrollSmoother.create({
+    wrapper: ".wrapper",
+    content: ".content",
+    smooth: 1.5,
+    effects: true,
+  });
+}
+
+gsap.fromTo(
+  ".hero-section",
+  { opacity: 1 },
+  {
+    opacity: 0,
+    scrollTrigger: {
+      trigger: ".hero-section",
+      start: "center",
+
+      scrub: true,
+    },
+  }
+);
